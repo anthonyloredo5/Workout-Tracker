cdabe48bc43521c6a6f670fecfb36f9425074c35
Anthony Loredo <anthonyloredo5@gmail.com>
2021-02-16 16:13:16 -0500

    Established mongodb connection

--- models/workout.js
+++ models/workout.js
@@ -18,7 +18,32 @@ const workoutSchema = new Schema({
             trim: true,
             require: "Enter an exercise name"
         },
-        
-
+        duration: {
+            type: Number,
+            trim: true,
+            require: "Enter an exercise duration"
+        },
+        weight: {
+            type: Number,
+            trim: true,
+            require: "Enter an exercise weight"
+        },
+        reps: {
+            type: Number,
+            trim: true,
+            require: "Enter a number of reps"
+        },
+        sets: {
+            type: Number,
+            trim: true,
+            require: "Enter a number of sets"
+        },
+        distance: {
+            type: Number,
+            trim: true,
+            require: "Enter a distance traveled"
+        }
     }
 })
+
+module.exports = workoutSchema;
--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
-const PORT = 3002;
+const bodyParser = require('body-parser');
+const cors =  require('cors');
+
+const PORT = process.env.PORT || 3002;
+const CONNECTION_URL = 'mongodb+srv://anthonyloredo5:anthonyloredo5123@cluster0.ts5pj.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 const app = express();
+
+//middle for mongoose
+app.use(bodyParser.json({ limit:"30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit:"30mb", extended: true }));
+app.use(cors());
 
 app.use(logger("dev"));
 
@@ -14,6 +23,9 @@ app.use(express.static('public'));
 app.use(require('./routes/htmlRoutes.js'));
 app.use(require('./routes/apiRoutes.js'));
 
-app.listen(PORT, () => {
-    console.log(`App listening on http://localhost:${PORT}`);
-});
+//db connection
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
