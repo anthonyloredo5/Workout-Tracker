675c2edf9f73c66124ea3dfe6a16c6d8c4aa21ab
Anthony Loredo <anthonyloredo5@gmail.com>
2021-02-16 22:37:54 -0500

    added env file to hide db

--- server.js
+++ server.js
@@ -3,11 +3,12 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors =  require('cors');
+const dotenv = require('dotenv');
 
 const PORT = process.env.PORT || 3002;
-const CONNECTION_URL = 'mongodb+srv://anthonyloredo5:anthonyloredo5123@cluster0.ts5pj.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
 const app = express();
+dotenv.config();
 
 //middle for mongoose
 app.use(bodyParser.json({ limit:"30mb", extended: true }));
@@ -24,7 +25,7 @@ app.use(require('./routes/htmlRoutes.js'));
 app.use(require('./routes/apiRoutes.js'));
 
 //db connection
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => app.listen(PORT, () => console.log(`App listening on http://localhost:${PORT}`)))
 .catch((error) => console.log(error.message));
 
